e34acb9029f9629f7ce554bd2e6c4beb407cd69b
Derek Marion <derek.j.marion@gmail.com>
2023-10-05 12:43:25 -0700

    working funtions for part I in python

--- python/character_match.py
+++ python/character_match.py
@@ -2,9 +2,15 @@
 
 # Part 1
 def is_character_match(string1, string2):
-	# Your code here
-	pass
+	if sorted(string1.lower()) == sorted(string2.lower()):
+		return True
+	return False
 
+def is_character_match_complex(string1, string2):
+	for character in string1:
+		if character.lower() not in string2.lower():
+			return False
+		return True
 
 # Part 2
 def anagrams_for(word, list_of_words):
